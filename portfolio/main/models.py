fbeb3d88e7ffcc44f73d78d52b970f7cbc27089c
Shani Henriques <shanihenriques@gmail.com>
2022-01-23 19:38:40 +0000

    Added testimonial and certificate models

--- portfolio/main/models.py
+++ portfolio/main/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 from ckeditor.fields import RichTextField
 
+''' ATTRIBUTES '''
+
 class Skill(models.Model):
     class Meta:
         verbose_name_plural = 'Skills'
@@ -12,9 +14,39 @@ class Skill(models.Model):
     name = models.CharField(max_length=20, blank=True, null=True)
     score = models.IntegerField(default=80, blank=True, null=True)
     image = models.FileField(blank=True, null=True, upload_to="skills")
-    # true = key skills, false = other skill
+    # true = key skills, false = coding skill
     is_key_skill = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
 
+class Testimonial(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Testimonials'
+        verbose_name = 'Testimonial'
+        ordering = ["name"]
+
+    thumbnail = models.ImageField(blank=True, null=True, upload_to="testimonials")
+    name = models.CharField(max_length=200, blank=True, null=True)
+    role = models.CharField(max_length=200, blank=True, null=True)
+    quote = models.CharField(max_length=500, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class Certificate(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Certificates'
+        verbose_name = 'Certificate'
+
+    date = models.DateTimeField(blank=True, null=True)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    title = models.CharField(max_length=200, blank=True, null=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
