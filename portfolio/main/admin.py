fdc6f7a454162b487b09dd691a3a4aaca1a530ef
Shani Henriques <shanihenriques@gmail.com>
2022-01-23 21:59:57 +0000

    Registered all models in admin

--- portfolio/main/admin.py
+++ portfolio/main/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import (
+    UserProfile,
+    ContactProfile,
+    Testimonial,
+    Media,
+    PortfolioEntry,
+    BlogPost,
+    Certificate,
+    Skill
+)
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+	list_display = ('id', 'user')
+
+@admin.register(ContactProfile)
+class ContactAdmin(admin.ModelAdmin):
+	list_display = ('id', 'timestamp', 'name',)
+
+@admin.register(PortfolioEntry)
+class PortfolioEntryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_active')
+    readonly_fields = ('slug',)
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_active')
+    readonly_fields = ('slug',)
+
+@admin.register(Certificate)
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'score')
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'is_active')
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
