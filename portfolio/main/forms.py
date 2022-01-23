4da3f02398c899c00fcdbfd6d95f0102153fb819
Shani Henriques <shanihenriques@gmail.com>
2022-01-23 22:42:38 +0000

    Set up contact form

---
+++ portfolio/main/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import ContactProfile
+
+
+class ContactForm(forms.ModelForm):
+    # form input fields for the frontend 
+	name = forms.CharField(max_length=100, required=True,
+		widget=forms.TextInput(attrs={
+			'placeholder': '*Full name..',
+            # comment if not using form control in templates
+			'class': 'form-control'
+		}))
+	email = forms.EmailField(max_length=254, required=True, 
+		widget=forms.TextInput(attrs={
+			'placeholder': '*Email..',
+			'class': 'form-control'
+		}))
+	message = forms.CharField(max_length=1000, required=True, 
+		widget=forms.Textarea(attrs={
+			'placeholder': '*Message..',
+			'class': 'form-control',
+            # update this if template changes
+			'rows': 6,
+		}))
+
+	class Meta:
+		model = ContactProfile
+		fields = ('name', 'email', 'message',)
