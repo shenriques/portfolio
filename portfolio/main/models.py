fd1eab2d7d2e26130d7199c0b8b9b93dc74f0c46
Shani Henriques <shanihenriques@gmail.com>
2022-01-23 19:44:18 +0000

    Improved certificate model

--- portfolio/main/models.py
+++ portfolio/main/models.py
@@ -43,8 +43,8 @@ class Certificate(models.Model):
         verbose_name = 'Certificate'
 
     date = models.DateTimeField(blank=True, null=True)
-    name = models.CharField(max_length=50, blank=True, null=True)
-    title = models.CharField(max_length=200, blank=True, null=True)
+    title = models.CharField(max_length=50, blank=True, null=True)
+    issued_by = models.CharField(max_length=200, blank=True, null=True)
     description = models.CharField(max_length=500, blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
