e73c91ca6f011dbc8fecbde33637c27102e72cd7
Shani Henriques <shanihenriques@gmail.com>
2022-01-23 22:25:25 +0000

    Import signals to ready() so it works when app starts

--- portfolio/main/apps.py
+++ portfolio/main/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    # import signals when the main app start
+    def ready(self):
+        import main.signals 
