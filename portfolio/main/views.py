fdafa865c44510626abaf2936bfee2777fee1f41
Shani Henriques <shanihenriques@gmail.com>
2022-01-24 20:25:57 +0000

    Added homepage view

--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.views import generic
+from . forms import ContactForm
+from . models import (
+		UserProfile,
+		BlogPost,
+		PortfolioEntry,
+		Testimonial,
+		Certificate
+	)
 
-# Create your views here.
+# Homepage
+class IndexView(generic.TemplateView):
+	template_name = "main/index.html"
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		
+		# only retrieve lists of the active data 
+		testimonials = Testimonial.objects.filter(is_active=True)
+		certificates = Certificate.objects.filter(is_active=True)
+		blog_posts = BlogPost.objects.filter(is_active=True)
+		portfolio_entries = PortfolioEntry.objects.filter(is_active=True)
+		
+		# enable home page to access content objects
+		context["testimonials"] = testimonials
+		context["certificates"] = certificates
+		context["blog_posts"] = blog_posts
+		context["portfolio_entries"] = portfolio_entries
+		
+		return context
