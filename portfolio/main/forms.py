558d94f7e8bea650d38dc08d916983075d1e0cf8
Shani Henriques <shanihenriques@gmail.com>
2022-01-27 19:24:45 +0000

    Set up contact page to have a django form that creates a contact profile when message is submitted

--- portfolio/main/forms.py
+++ portfolio/main/forms.py
@@ -6,18 +6,18 @@ class ContactForm(forms.ModelForm):
     # form input fields for the frontend 
 	name = forms.CharField(max_length=100, required=True,
 		widget=forms.TextInput(attrs={
-			'placeholder': '*Full name..',
+			'placeholder': 'Full name...',
             # comment if not using form control in templates
 			'class': 'form-control'
 		}))
 	email = forms.EmailField(max_length=254, required=True, 
 		widget=forms.TextInput(attrs={
-			'placeholder': '*Email..',
+			'placeholder': 'Email...',
 			'class': 'form-control'
 		}))
 	message = forms.CharField(max_length=1000, required=True, 
 		widget=forms.Textarea(attrs={
-			'placeholder': '*Message..',
+			'placeholder': 'Message...',
 			'class': 'form-control',
             # update this if template changes
 			'rows': 6,
